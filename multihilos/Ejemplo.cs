12ca970968e389254f23b319bb3ecc88dd03839b
Andrea Evelyn Mejía Rubio <andiemartin33@gmail.com>
2021-03-22 10:05:47 -0600

    Agregar archivos de proyecto.

---
+++ multihilos/Ejemplo.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace multihilos
+{
+    class Ejemplo
+    {
+        public void Proceso1()
+        {
+            for (int i = 0; i < 60; i++)
+            {
+                Console.WriteLine("Hilo 1="+i);
+              
+            }
+           
+
+
+        }
+
+        public void Proceso2()
+        {
+            for (int i = 0; i < 60; i++)
+            {
+                Console.WriteLine("Hilo 2=" + i);
+
+            }
+
+        }
+
+        public void Proceso3()
+        {
+            for (int i = 0; i < 60; i++)
+            {
+                Console.WriteLine("Hilo 3=" + i);
+
+            }
+
+        }
+
+
+    }
+}
---
+++ multihilos/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+
+namespace multihilos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Ejemplo obj = new Ejemplo();
+
+            //crear hilos
+
+            Thread h1 = new Thread(new ThreadStart(obj.Proceso1));
+            Thread h2 = new Thread(new ThreadStart(obj.Proceso2));
+            Thread h3 = new Thread(new ThreadStart(obj.Proceso3));
+
+            //inicializar hilo
+            h1.Start();
+            h2.Start();
+            h3.Start();
+
+            //cerrar hilos
+            h1.Join();
+            h2.Join();
+            h3.Join();
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
